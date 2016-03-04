4fabd5e6de21c097d80369d27b7cad3f9ec7d54f
ailu <a1lu@arcor.de>
2016-03-04 11:19:25 +0100

    fixup! extended globbing matcher

--- src/string/ext.c
+++ src/string/ext.c
@@ -163,6 +163,8 @@ struct context globmatchS(struct context con)
      * never a need to backtrack multiple levels.
      */
     char const *back_pat = con.back_pat, *back_str = con.back_str;
+    char const *repeat_pat= con.repeat? con.pat : NULL;
+    char const *repeat_str=NULL;
     /*
      * Loop over each token (character or class) in pat, matching
      * it against the remaining unmatched tail of str.  Return false
@@ -201,17 +203,38 @@ struct context globmatchS(struct context con)
         /*printf("p: %c , c: %c\n",p,c);*/
         if(p==')' || p=='|')
         {
+                puts(") or |");
+            if(repeat_pat)
+            {
+                puts("repeat");
+                repeat_str=con.str-1;
+            }
             if(con.pat_chunk_end)
+            {
                 con.pat=con.pat_chunk_end;
-            p = *con.pat++;
-            puts("klammer oder |");
-            /*printf("c: %c p: %c\n", c , p);*/
+                p = *con.pat++;
+                printf("c: %c p: %c\n", c , p);
+                /*con.except=0;*/
+            }
+            /*con.except=0;*/
+            if(con.except)
+            {
+                puts("! pattern matched");
+                return (struct context){.matched=1, .str=con.str};
+            }
         }
+        /// TODO: malformed bracket strings
+            /*else if (p==')')*/
+            /*{*/
+                /*puts("unexpected word \')\'");*/
+                /*exit(1);*/
+            /*}*/
+            /*printf("c: %c p: %c\n", c , p);*/
         switch (p) {
             /*case '|':*/
-                if(con.pat_chunk_end)
-                    con.pat=con.pat_chunk_end;
-                con.str--;
+                /*if(con.pat_chunk_end)*/
+                    /*con.pat=con.pat_chunk_end;*/
+                /*con.str--;*/
                 /*printf("count %d\n",count);*/
                       /*puts("ende |");*/
                       /*while(count>0)*/
@@ -228,47 +251,82 @@ struct context globmatchS(struct context con)
                       /*break;*/
                       /*return str;*/
                 /*break;*/
-            case '?':   /* Wildcard: anything but nul */
-                    if(con.pat[0]=='(')
+            case '?':
+                /* Matches zero or one occurence of the given patterns */
+                if(con.pat[0]=='(')
+                {
+                    /*printf("aa: c: %c p: %c\n",c ,p);*/
+                    /*if(c == '\0')*/
+                    /*return (struct context){.matched=1};*/
+                    puts("?");
+                    /*printf("%s\n",pat+1);*/
+                    --con.str;
+                    struct ext_glob eg=findNextS((struct ext_glob){.cur_pat=con.pat});
+                    char const *pat_end=eg.end_pat > con.pat_chunk_end ?
+                        eg.end_pat: con.pat_chunk_end;
+                    struct context res;
+                    while(!eg.ret)
                     {
-                        /*printf("aa: c: %c p: %c\n",c ,p);*/
-                        if(c == '\0')
-                            return (struct context){.matched=1};
-                        puts("?");
-                        /*printf("%s\n",pat+1);*/
-                        --con.str;
-                        struct ext_glob eg=findNextS((struct ext_glob){.cur_pat=con.pat});
-                        char const *pat_end=eg.end_pat > con.pat_chunk_end ?
-                            eg.end_pat: con.pat_chunk_end;
-                        struct context res;
-                        while(!eg.ret)
-                        {
-                            struct context arg={.str=con.str, .pat=eg.cur_pat,
-                                .pat_chunk_end=pat_end, .back_pat=back_pat,
+                        struct context arg={.str=con.str, .pat=eg.cur_pat,
+                            .pat_chunk_end=pat_end, .back_pat=back_pat,
                             .back_str=back_str};
-                            /*printContext(arg);*/
-                            res=globmatchS(arg);
-                            /*printContext(res);*/
-                            if(res.matched==1)
-                            {
-                                /*puts("rec matched");*/
-                                return res;
-                            }
-                            eg=findNextS(eg);
+                        /*printContext(arg);*/
+                        res=globmatchS(arg);
+                        /*printContext(res);*/
+                        if(res.matched==1)
+                        {
+                            /*puts("rec matched");*/
+                            return res;
                         }
-                        con.pat=pat_end;
-                        /*printf("str: %s pat: %s \n",con.str, con.pat);*/
-                        p=*con.pat++;
-                        c=*con.str++;
-                        /*printf("c: %c p: %c\n",c ,p);*/
-                        goto literal;
-                    }else
-                    {
-                        if (c == '\0')
-                            return (struct context){.matched=0};
+                        eg=findNextS(eg);
                     }
+                    con.pat=pat_end;
+                    printf("str: %s pat: %s \n",con.str, con.pat);
+                    /*p=*con.pat++;*/
+                    /*c=*con.str++;*/
+                    /*printf("c: %c p: %c\n",c ,p);*/
+                    /*goto literal;*/
+                }else   /* Wildcard: anything but nul */
+                {
+                    if (c == '\0')
+                        return (struct context){.matched=0, .str=con.str};
+                }
                 break;
             case '@':
+                /* Matches one of the given patterns */
+                if(con.pat[0]=='(')
+                {
+                    /*printf("aa: c: %c p: %c\n",c ,p);*/
+                    if(c == '\0') /// TODO check this
+                        return (struct context){.matched=0, .str=con.str};
+                    puts("@");
+                    /*printf("%s\n",pat+1);*/
+                    --con.str;
+                    struct ext_glob eg=findNextS((struct ext_glob){.cur_pat=con.pat});
+                    char const *pat_end=eg.end_pat > con.pat_chunk_end ?
+                        eg.end_pat: con.pat_chunk_end;
+                    struct context res;
+                    while(!eg.ret)
+                    {
+                        struct context arg={.str=con.str, .pat=eg.cur_pat,
+                            .pat_chunk_end=pat_end, .back_pat=back_pat,
+                            .back_str=back_str};
+                        /*printContext(arg);*/
+                        res=globmatchS(arg);
+                        /*printContext(res);*/
+                        if(res.matched==1)
+                        {
+                            /*puts("rec matched");*/
+                            return res;
+                        }
+                        eg=findNextS(eg);
+                    }
+                    return res;
+                }else
+                {
+                    /// TODO malformed string
+                    goto literal;
+                }
                     /*if(pat[0]=='(')*/
                     /*{*/
                         /*puts("@");*/
@@ -313,15 +371,127 @@ struct context globmatchS(struct context con)
                     /*}else goto literal;*/
 
                 break;
-            case '*':   /* Any-length wildcard */
-                if (*con.pat == '\0')   /* Optimize trailing * case */
+            case '*':
+                /* Matches zero or more occurences of the given patterns */
+                if(con.pat[0]=='(')
                 {
+                    /*printf("aa: c: %c p: %c\n",c ,p);*/
+                    /*if(c == '\0')*/
+                    /*return (struct context){.matched=1};*/
+                    puts("*");
+                    /*printf("%s\n",pat+1);*/
+                    --con.str;
+                    struct ext_glob eg=findNextS((struct ext_glob){.cur_pat=con.pat});
+                    char const *pat_end=eg.end_pat > con.pat_chunk_end ?
+                        eg.end_pat: con.pat_chunk_end;
+                    struct context res;
+                    while(!eg.ret)
+                    {
+                        struct context arg={.str=con.str, .pat=eg.cur_pat,
+                            .pat_chunk_end=pat_end, .back_pat=back_pat,
+                            .back_str=back_str, .repeat=1};
+                        /*printContext(arg);*/
+                        res=globmatchS(arg);
+                        /*printContext(res);*/
+                        if(res.matched==1)
+                        {
+                            /*puts("rec matched");*/
+                            return res;
+                        }
+                        eg=findNextS(eg);
+                    }
+                    con.pat=pat_end;
+                    printf("no match str: %s pat: %s \n",con.str, con.pat);
+                    /*p=*con.pat++;*/
+                    /*c=*con.str++;*/
+                    printf("c: %c p: %c\n",c ,p);
+                    /*goto literal;*/
+                }else /* Any-length wildcard */
+                {
+                    if (*con.pat == '\0')   /* Optimize trailing * case */
+                    {
+                        puts("stern");
+                        return (struct context){.matched=1, .str=con.str};
+                    }
                     puts("stern");
-                    return (struct context){.matched=1};
+                    back_pat = con.pat;
+                    back_str = --con.str;   /* Allow zero-length match */
                 }
-                puts("stern");
-                back_pat = con.pat;
-                back_str = --con.str;   /* Allow zero-length match */
+                break;
+            case '+':
+                /* Matches one or more of the given patterns */
+                if(con.pat[0]=='(')
+                {
+                    /*printf("aa: c: %c p: %c\n",c ,p);*/
+                    if(c == '\0') /// TODO check this
+                        return (struct context){.matched=0, .str=con.str};
+                    puts("+");
+                    /*printf("%s\n",pat+1);*/
+                    --con.str;
+                    struct ext_glob eg=findNextS((struct ext_glob){.cur_pat=con.pat});
+                    char const *pat_end=eg.end_pat > con.pat_chunk_end ?
+                        eg.end_pat: con.pat_chunk_end;
+                    struct context res;
+                    while(!eg.ret)
+                    {
+                        struct context arg={.str=con.str, .pat=eg.cur_pat,
+                            .pat_chunk_end=pat_end, .back_pat=back_pat,
+                            .back_str=back_str, .repeat=1};
+                        /*printContext(arg);*/
+                        res=globmatchS(arg);
+                        /*printContext(res);*/
+                        if(res.matched==1)
+                        {
+                            /*puts("rec matched");*/
+                            return res;
+                        }
+                        eg=findNextS(eg);
+                    }
+                    return res;
+                }else{
+                    /// TODO malformed string
+                    goto literal;
+                }
+                break;
+            case '!':
+                /* Matches anything except one of the given patterns */
+                if(con.pat[0]=='(')
+                {
+                    /*printf("aa: c: %c p: %c\n",c ,p);*/
+                    if(c == '\0') /// TODO check this
+                        return (struct context){.matched=1, .str=con.str};
+                    puts("!");
+                    /*printf("%s\n",pat+1);*/
+                    --con.str;
+                    struct ext_glob eg=findNextS((struct ext_glob){.cur_pat=con.pat});
+                    char const *pat_end=eg.end_pat > con.pat_chunk_end ?
+                        eg.end_pat: con.pat_chunk_end;
+                    struct context res;
+                    while(!eg.ret)
+                    {
+                        struct context arg={.str=con.str, .pat=eg.cur_pat,
+                            .pat_chunk_end=pat_end, .back_pat=back_pat,
+                            .back_str=back_str, .except=1};
+                        /*printContext(arg);*/
+                        res=globmatchS(arg);
+                        /*printContext(res);*/
+                        if(res.matched==1)
+                        {
+                            /*puts("rec matched");*/
+                            /*return res;*/
+                              return (struct context){.matched=0, .str=con.str};
+                            /*goto backtrack;*/
+                        }
+                        eg=findNextS(eg);
+                    }
+                    puts("ende !");
+                    printContext(res);
+                    con.pat=pat_end;
+                    con.str=res.str;
+                }else{
+                    /// TODO malformed string
+                    goto literal;
+                }
                 break;
             case '[': { /* Character class */
                           int match = 0, inverted = (*con.pat == '!');
@@ -355,18 +525,33 @@ struct context globmatchS(struct context con)
                       /*FALLTHROUGH*/
             default:    /* Literal character */
 literal:
+                      /*if ((c == p)^con.except) {*/
                       if (c == p) {
                           printf("match %c %c\n", c,p);
                           if (p == '\0')
                           {
                               puts("ende literal");
-                              return (struct context){.matched=1};
+                              return (struct context){.matched=1, .str=con.str};
                           }
+                          /*if(c!='\0') // TODO check this*/
+                              break;
+                      }else if(con.repeat && (con.pat == con.pat_chunk_end+1))
+                      {
+                          puts("reset pat");
+                          con.pat=repeat_pat;
+                          con.str=repeat_str;
+                          /*printf("%c %c %s %s\n", c,p, con.pat, con.str);*/
+                          if(!con.str)
+                              return (struct context){.matched=0, .str=con.str};
                           break;
                       }
 backtrack:
+                      /*printf("%p %p\n", con.pat, con.pat_chunk_end);*/
                       if (c == '\0' || !back_pat)
-                          return (struct context){.matched=0};
+                      {
+                          puts("ret backtrack");
+                          return (struct context){.matched=0, .str=con.str};
+                      }
                           /* No point continuing */
                       /* Try again from last *, one character later in str. */
                       con.pat = back_pat;
